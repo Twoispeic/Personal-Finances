434de411e3225689a45ef7e544c5ac8c4de52a03
twozeroepicDEV <90443957+Twoispeic@users.noreply.github.com>
2026-07-25 21:10:17 +0700

    Revert "Bao"

--- src/goals/MucTieu.cpp
+++ src/goals/MucTieu.cpp
@@ -1,12 +1,3 @@
 #include "MucTieu.h"
 
-MucTieu::MucTieu() {
-    this->tenMucTieu = "";
-    this->soTienMucTieu = 0.0;
-    this->soTienDaTietKiem = 0.0;
-    this->trangThai = nullptr;
-    this->chienLuoc = nullptr;
-}
-
-MucTieu::~MucTieu() {
-}
+MucTieu::MucTieu() {}
--- src/models/NguoiDung.cpp
+++ src/models/NguoiDung.cpp
@@ -1,17 +1,3 @@
 #include "NguoiDung.h"
 
-NguoiDung::NguoiDung() {
-    this->ten = "";
-    this->congViec = "";
-}
-
-NguoiDung::NguoiDung(QString tenND, QString congViecND) {
-    this->ten = tenND;
-    this->congViec = congViecND;
-}
-
-QString NguoiDung::getTen() const { return ten; }
-void NguoiDung::setTen(const QString &tenND) { this->ten = tenND; }
-
-QString NguoiDung::getCongViec() const { return congViec; }
-void NguoiDung::setCongViec(const QString &congViecND) { this->congViec = congViecND; }
+Account::Account() {}
