c43590b25f0a46027bf66675454c54517e1e005e
twozeroepicDEV <dogegamin456@gmail.com>
2026-07-28 08:49:53 +0700

    Next finalization

--- src/goals/MucTieuNganHan.cpp
+++ src/goals/MucTieuNganHan.cpp
@@ -1,9 +1,5 @@
 #include "MucTieuNganHan.h"
 
-MucTieuNganHan::MucTieuNganHan() {
-    this->thoiHanThang = 0;
-}
-
 MucTieuNganHan::MucTieuNganHan(const QString& ten, double soTienMucTieu, int thoiHanThang)
     : MucTieu(ten, soTienMucTieu), thoiHanThang(thoiHanThang) {}
 
