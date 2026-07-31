0cc00ace68444ff376bab7c1f66ed60d47bf781a
Thanhhung3006 <thanhhungv86@gmail.com>
2026-07-31 19:00:15 +0700

    tao 3 files muctieutaichinhwidget, muctieucardwidget va tao muctieuwidget

--- src/gui/goals/taomuctieudialog.cpp
+++ src/gui/goals/taomuctieudialog.cpp
@@ -1,14 +1,43 @@
 #include "taomuctieudialog.h"
 #include "ui_taomuctieudialog.h"
+#include <QDebug>
 
-taomuctieudialog::taomuctieudialog(QWidget *parent)
-    : QWidget(parent)
+TaoMucTieuDialog::TaoMucTieuDialog(QWidget *parent)
+    : QDialog(parent)
     , ui(new Ui::taomuctieudialog)
 {
     ui->setupUi(this);
+
+    // Gán giá trị mặc định lúc mới mở form
+    dangONganHan = true;
 }
 
-taomuctieudialog::~taomuctieudialog()
+TaoMucTieuDialog::~TaoMucTieuDialog()
 {
     delete ui;
+}
+
+bool TaoMucTieuDialog::laNganHan() const {
+    return dangONganHan;
+}
+
+QString TaoMucTieuDialog::layTen() const {
+    // TODO: Lấy chữ từ ô nhập liệu, vd: return ui->txtTenMucTieu->text();
+    return QString("");
+}
+
+double TaoMucTieuDialog::laySoTienMucTieu() const {
+    return 0.0; // TODO: return ui->spinSoTien->value();
+}
+
+int TaoMucTieuDialog::layThoiHanThang() const {
+    return 0; // TODO: Lấy từ ô nhập thời hạn
+}
+
+int TaoMucTieuDialog::laySoKyTraGop() const {
+    return 0; // TODO: Lấy số kỳ trả góp
+}
+
+double TaoMucTieuDialog::laySoTienMoiKy() const {
+    return 0.0; // TODO: Lấy số tiền mỗi kỳ
 }
