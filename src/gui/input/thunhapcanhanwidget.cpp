c5c3b61f1e1372d35e14d650e1b7fb189ff36c08
Thanhhung3006 <thanhhungv86@gmail.com>
2026-07-31 13:34:52 +0700

    bootylover69

--- src/gui/input/thunhapcanhanwidget.cpp
+++ src/gui/input/thunhapcanhanwidget.cpp
@@ -1,14 +1,34 @@
 #include "thunhapcanhanwidget.h"
 #include "ui_thunhapcanhanwidget.h"
+#include <QDebug> // Thêm thư viện để in log
 
-thunhapcanhanwidget::thunhapcanhanwidget(QWidget *parent)
+ThuNhapCaNhanWidget::ThuNhapCaNhanWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::thunhapcanhanwidget)
 {
     ui->setupUi(this);
+
+    // TODO: Kết nối nút bấm lưu thu nhập trên giao diện với hàm xử lý
+    // connect(ui->btnLuuThuNhap, &QPushButton::clicked, this, &ThuNhapCaNhanWidget::onLuuThuNhapClicked);
 }
 
-thunhapcanhanwidget::~thunhapcanhanwidget()
+ThuNhapCaNhanWidget::~ThuNhapCaNhanWidget()
 {
     delete ui;
+}
+
+void ThuNhapCaNhanWidget::hienThiSoTienThangHienTai(double soTien)
+{
+    // TODO: Gắn số tiền lên Label trên giao diện
+    // ui->lblSoTien->setText(QString::number(soTien));
+    qDebug() << "Đang hiển thị thu nhập tháng hiện tại:" << soTien;
+}
+
+void ThuNhapCaNhanWidget::onLuuThuNhapClicked()
+{
+    // TODO: Lấy dữ liệu từ ô nhập, lưu vào Database
+    qDebug() << "Đã bấm nút Lưu thu nhập!";
+
+    // Phát tín hiệu báo cho các màn hình khác biết dữ liệu đã thay đổi
+    emit duLieuThayDoi();
 }
--- src/gui/input/trangchuwidget.cpp
+++ src/gui/input/trangchuwidget.cpp
@@ -1,14 +1,45 @@
 #include "trangchuwidget.h"
 #include "ui_trangchuwidget.h"
+#include "bieudowidget.h" // Gọi thư viện BieuDoWidget vào để sử dụng
+#include <QDebug>         // Dùng để in log kiểm tra lúc chạy
 
-trangchuwidget::trangchuwidget(QWidget *parent)
+TrangChuWidget::TrangChuWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::trangchuwidget)
 {
     ui->setupUi(this);
+
+    // TODO: Khởi tạo các thành phần giao diện khác nếu cần
+    // bieuDoThuNho = new BieuDoWidget(this);
+
+    // TODO: Kết nối nút bấm "Kết thúc tháng" trên file .ui với hàm xử lý
+    // connect(ui->btnKetThucThang, &QPushButton::clicked, this, &TrangChuWidget::onKetThucThangClicked);
 }
 
-trangchuwidget::~trangchuwidget()
+TrangChuWidget::~TrangChuWidget()
 {
+    // Dọn dẹp RAM khi đóng widget
     delete ui;
+}
+
+void TrangChuWidget::capNhatTongQuan(NguoiDung* nd)
+{
+    // Kiểm tra an toàn: Nếu không có dữ liệu người dùng thì thoát luôn, tránh crash app
+    if (nd == nullptr) {
+        qDebug() << "Chưa có dữ liệu người dùng!";
+        return;
+    }
+
+    // TODO: Cập nhật các con số lên giao diện dựa vào dữ liệu của 'nd'
+    // Ví dụ: ui->lblTongTien->setText(QString::number(nd->getTongThuNhap()));
+    qDebug() << "Đang cập nhật tổng quan giao diện...";
+}
+
+void TrangChuWidget::onKetThucThangClicked()
+{
+    // TODO: Viết code tính toán, lưu dữ liệu vào database khi hết tháng
+    qDebug() << "Nút kết thúc tháng đã được bấm!";
+
+    // Sau khi tính toán xong, phát tín hiệu (signal) để báo cho các widget khác biết dữ liệu đã đổi
+    emit duLieuThayDoi();
 }
