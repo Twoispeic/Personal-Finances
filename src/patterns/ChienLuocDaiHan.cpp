a2fe8794004d625055ab42d84df8d3120b808006
twozeroepicDEV <dogegamin456@gmail.com>
2026-08-10 14:31:12 +0700

    add comments revert

--- src/patterns/ChienLuocDaiHan.cpp
+++ src/patterns/ChienLuocDaiHan.cpp
@@ -5,8 +5,5 @@ ChienLuocDaiHan::ChienLuocDaiHan(double soTienMoiThang) : soTienTraGopMoiThang(s
 
 double ChienLuocDaiHan::phanBoTien(double soTienDu, double soTienConThieu) {
     double muonLay = std::min(soTienTraGopMoiThang, soTienConThieu);
-    double conLaiSauKhiTra = soTienDu - muonLay;
-    if (conLaiSauKhiTra < muonLay * 0.1) return 0.0;
-
     return std::min(soTienDu, muonLay);
 }
